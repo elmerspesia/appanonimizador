73694b4cf4fed2bf63bfecdddf7de1f4726a2b02
elmerspesia <elmer.dotti@spesia.com.br>
2025-05-08 12:47:06 -0300

    Create oracle_connector.py

---
+++ .anon_utils/oracle_connector.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import cx_Oracle
+
+def test_oracle_connection(host, port, service_name, user, password):
+    dsn = cx_Oracle.makedsn(host, port, service_name=service_name)
+    try:
+        with cx_Oracle.connect(user=user, password=password, dsn=dsn) as connection:
+            return True
+    except Exception:
+        return False
+
+def load_data_from_oracle(host, port, service_name, user, password, query="SELECT * FROM YOUR_TABLE"):
+    dsn = cx_Oracle.makedsn(host, port, service_name=service_name)
+    with cx_Oracle.connect(user=user, password=password, dsn=dsn) as connection:
+        df = pd.read_sql(query, con=connection)
+    return df
